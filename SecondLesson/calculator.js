ed3e5068164a553a78eefc8ce71eb23c4d4b9127
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2020-03-17 11:14:46 +0300

    Added some new functions to calculator
    Signed-off-by: Sirojiddin Saidmurodov <siroj.serj15@outlook.com>

--- SecondLesson/calculator.js
+++ SecondLesson/calculator.js
@@ -1,8 +1,39 @@
+function read(x) {
+    return parseFloat(document.getElementById(x).value);
+}
+
 function sum() {
-    let a = parseInt(document.getElementById("a").value);
-    let b = parseInt(document.getElementById("b").value);
+    let a = read("a");
+    let b = read("b");
     document.getElementById("result").innerText = a + b;
 }
+
+function sub() {
+    let a = read("a");
+    let b = read("b");
+    document.getElementById("result").innerText = a - b;
+}
+
+function multiply() {
+    let a = read("a");
+    let b = read("b");
+    document.getElementById("result").innerText = a * b;
+}
+
+function divide() {
+    let a = read("a");
+    let b = read("b");
+    document.getElementById("result").innerText = a / b;
+}
+
+function mod() {
+    let a = read("a");
+    let b = read("b");
+    document.getElementById("result").innerText = a % b;
+
+}
+
+
 
 let a = ["ОПиПК", "Культурология", "Матстат", "ОИБ", "ООАиП"];
 console.log(...a);
